224e2bef2406059154f54a47bc1421e74c75f1db
saurabh <saurabhjain04r@gmail.com>
2023-04-26 20:07:01 +0530

    frontend component completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,53 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-function App() {
+// import all components
+
+import Username from "./components/Username";
+import Password from "./components/Password";
+import Register from "./components/Register";
+import Profile from "./components/Profile";
+import Recovery from "./components/Recovery";
+import PageNotFound from "./components/PageNotFound";
+import Reset from "./components/Reset";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Username></Username>,
+  },
+  {
+    path: "/register",
+    element: <Register></Register>,
+  },
+  {
+    path: "/password",
+    element: <Password></Password>,
+  },
+  {
+    path: "/profile",
+    element: <Profile></Profile>,
+  },
+  {
+    path: "/recovery",
+    element: <Recovery></Recovery>,
+  },
+  {
+    path: "/reset",
+    element: <Reset></Reset>,
+  },
+  {
+    path: "*",
+    element: <PageNotFound></PageNotFound>,
+  },
+]);
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>React app</h1>
-    </div>
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   );
-}
+};
 
 export default App;
